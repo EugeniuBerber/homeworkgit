c2d7eeaf90ba15874a6184498332b8a5bf53f341
EugeniuBerber <75701425+EugeniuBerber@users.noreply.github.com>
2021-08-12 13:11:36 -0600

    function was changed to print odd numbers from 1 to 100

--- src/numbersToPrint.java
+++ src/numbersToPrint.java
@@ -1,6 +1,7 @@
 public class numbersToPrint {
     public static void main(String[] args) {
-        for (int i=1; i<=100;i++)
+        for (int i=1; i<=100;i+=2)
             System.out.println(i);
+
     }
 }
